7918e9689905fabb43e35d271e7891265dd6b575
Kyle Blaney <blaney.kyle@gmail.com>
2013-02-19 15:32:20 -0500

    Improve report formatting.

--- src/main/java/com/kblaney/hockey/GameReportsToGoalsSummaryFunctionImpl.java
+++ src/main/java/com/kblaney/hockey/GameReportsToGoalsSummaryFunctionImpl.java
@@ -41,6 +41,12 @@ final class GameReportsToGoalsSummaryFunctionImpl implements GameReportsToGoalsS
       s.append(": ");
       s.append(goalReport.getGoalDescription());
       s.append('\n');
+      s.append("Plus players: ");
+      s.append(goalReport.getPlusPlayers());
+      s.append('\n');
+      s.append("Minus players: ");
+      s.append(goalReport.getMinusPlayers());
+      s.append('\n');
     }
     return s.toString();
   }
